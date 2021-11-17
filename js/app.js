6a19f86cc9a0601a76c729b9cf612cebc7765221
MattHill94 <matty2712@hotmail.co.uk>
2021-11-17 16:22:19 +0000

    index and css added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-const urlAPI = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone,dob &noinfo &nat=US'
+const api = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone,dob &noinfo &nat=US'
